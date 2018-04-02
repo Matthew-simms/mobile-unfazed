69f77e9152022c94482798c29b2f58dc6a7d7d70
matt <you@example.com>
2018-04-02 01:16:44 +0100

    types

--- actions/types.js
+++ actions/types.js
@@ -2,3 +2,4 @@ export const CAMERA_DATA = 'camera_data';
 export const CAMERA_OR_VIDEO = 'camera_or_video';
 export const EVENT_DATA = 'event_data';
 export const IS_LOADING = 'is_loading';
+export const LOGIN_DATA = 'login_data'
