df53f1b774b624ae34ede5c012d4b6c2836f6432
matt <you@example.com>
2018-04-02 01:14:54 +0100

    reducer signin

---
+++ actions/User.js
@@ -0,0 +1,20 @@
+import {  LOGIN_DATA } from './types';
+
+export const login = (username) => {
+    return {
+        type: 'LOGIN',
+        username: username,
+    };
+};
+
+export const loginFb = (userInfo) => {
+    return {
+        type: 'LOGINFB',
+        userInfo: userInfo
+    };
+};
+
+export const signup = (username) => {
+    return (dispatch) => {
+    };
+};
