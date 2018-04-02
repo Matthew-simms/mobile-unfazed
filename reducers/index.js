123dc67cd1d469ec6e60ae3fd60e8ec5b44f05d3
matt <you@example.com>
2018-04-02 01:21:32 +0100

    index reducers

--- reducers/index.js
+++ reducers/index.js
@@ -3,10 +3,12 @@ import VideoReducer from './VideoReducer';
 import CamDataReducer from './CamData';
 import EventData from './Event';
 import UniversalLoadingRed from './Universal_Loading';
+import UserData from './User';
 
 export default combineReducers({
   videoReducer: VideoReducer,
   camDataReducer: CamDataReducer,
   eventDataReducer: EventData,
-  universalLoadingRed: UniversalLoadingRed
+  universalLoadingRed: UniversalLoadingRed,
+  UserData: UserData
 });
